8f89f36692fb8cb5672eda65c260d23af5bdfac5
Tyson Tu <thaisontunguyen@gmail.com>
2021-10-13 00:28:00 -0500

    Create the window and add tomato

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+import time
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+# Creating a new window and configurations
+window = Tk()
+window.title('Simplex Pomodoro')
+window_height = 446
+window_width = 420
+window.minsize(width=window_width, height=window_height)
+window.config(padx=100, pady=100, bg=YELLOW)
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+x_coordinate = int(screen_width / 2) - int(window_width / 2)
+y_coordinate = int(screen_height / 2) - int(window_height / 2)
+window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_coordinate, y_coordinate))
+
+# Import background picture using .place
+tomato = PhotoImage(file='tomato.png')
+# background_label = Label(window, image=tomato)
+# background_label.place(x=0, y=0, relheight=1, relwidth=1)
+
+# Canvas
+canvas = Canvas(width=window_width, height=window_height, bg=YELLOW, highlightthickness=0)
+canvas.create_image(window_width//4, window_height//4+10, image=tomato)
+canvas.create_text(window_width//4, window_height//4+30, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
+canvas.pack()
+
+# Add time
+# time_label = Label(window, text="00:00", font=FONT_NAME)
+# time_label.config
+# time_label.place(relx=0.5, rely=0.5, anchor="center")
+
+# Main loop
+window.mainloop()
